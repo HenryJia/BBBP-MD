5e438deb3ebea942a26285988543514a8d006e10
Henry Jia <matgms@login01.avon.hpc>
2024-10-26 07:42:16 +0100

    Get PLUMED equilibration working maybe?

--- basolateral/gabriele/isopropanol/insert-molecule.sh
+++ basolateral/gabriele/isopropanol/insert-molecule.sh
@@ -1,1 +1,1 @@
-gmx insert-molecules -f md.gro -ci isopropanol.pdb -ip location.dat -o setup.gro -nmol 1 -replace TIP3
+gmx insert-molecules -f md.gro -ci ligandrm.pdb -ip location.dat -o setup.gro -nmol 1 -replace TIP3
