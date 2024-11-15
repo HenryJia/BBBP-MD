0e42f69ef5d87e47857b1e0c54b5418c20a01226
Hengjian Jia <matgms@login01.sulis.hpc>
2024-11-15 15:32:12 +0000

    push latest progress from sulis

--- apical/charmm-gui-2960669761-charmm36-nacl/gromacs/isopropanol/insert-molecule.sh
+++ apical/charmm-gui-2960669761-charmm36-nacl/gromacs/isopropanol/insert-molecule.sh
@@ -1,1 +1,5 @@
+module purge
+module load GCC/12.3.0  OpenMPI/4.1.5
+module load GROMACS/2023.3-CUDA-12.1.1-PLUMED-2.9.0
+
 gmx insert-molecules -f step7_production.gro -ci ligandrm.pdb -ip location.dat -o setup.gro -nmol 1 -replace TIP3
