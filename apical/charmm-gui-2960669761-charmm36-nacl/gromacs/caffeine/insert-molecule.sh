245e5ce9f34d5174bb6807997ecbdbcc30c8ff68
Henry Jia <matgms@login01.avon.hpc>
2024-11-29 08:09:32 +0000

    Rerun everything with parameterisations via sdf from pubchem processed by charmmgui

--- apical/charmm-gui-2960669761-charmm36-nacl/gromacs/caffeine/insert-molecule.sh
+++ apical/charmm-gui-2960669761-charmm36-nacl/gromacs/caffeine/insert-molecule.sh
@@ -1,5 +1,5 @@
 module purge
-module load GCC/11.2.0 OpenMPI/4.1.1
-module load GROMACS/2021.5-CUDA-11.4.1-PLUMED-2.8.0
+module load GCC/12.3.0  OpenMPI/4.1.5
+module load GROMACS/2023.3-CUDA-12.1.1-PLUMED-2.9.0
 
-gmx insert-molecules -f step7_production.gro -ci ligandrm.pdb -ip location.dat -o setup.gro -nmol 1 -replace TIP3
+gmx insert-molecules -f step7_production.gro -ci ligandrm.pdb -ip location.dat -o setup.gro -nmol 1 -replace TIP3 -scale 0.75 # use a larger scale because equilibrium seems to fail sometimes
