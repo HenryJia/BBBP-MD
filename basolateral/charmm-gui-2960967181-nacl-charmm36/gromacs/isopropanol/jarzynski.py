4949dda4b9116e991cc800d0459bea3976416ef1
Hengjian Jia <henryjia18@gmail.com>
2024-12-05 05:02:50 +0000

    try flipping the free energy around, because I think plumed is negating the free energy?

--- basolateral/charmm-gui-2960967181-nacl-charmm36/gromacs/isopropanol/jarzynski.py
+++ basolateral/charmm-gui-2960967181-nacl-charmm36/gromacs/isopropanol/jarzynski.py
@@ -5,6 +5,7 @@ import argparse
 import numpy as np
 import pandas as pd
 from sklearn.kernel_ridge import KernelRidge
+from sklearn.svm import SVR
 from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import root_mean_squared_error
 from sklearn.model_selection import KFold
@@ -14,7 +15,7 @@ import matplotlib.pyplot as plt
 # Parse command line arguments
 parser = argparse.ArgumentParser(description='Generate Plumed 2.8 configuration file for Jarzynski sampling')
 parser.add_argument('--fe', type=str, nargs='+', help='Free energy file(s) in Plumed output format')
-parser.add_argument('--cv', type=str, help='Collective variable file in Plumed output format')
+parser.add_argument('--cv', type=str, nargs='+', help='Collective variable file in Plumed output format')
 parser.add_argument('--template', type=str, required=True, help='Template file for Plumed configuration')
 parser.add_argument('--previous', type=str, required=True, help='A previous Plumed configuration file for us to pull the CVs from')
 parser.add_argument('--output', type=str, required=True, help='Output file for Plumed configuration')
@@ -23,6 +24,7 @@ args = parser.parse_args()
 
 assert hasattr(args, 'fe') or hasattr(args, 'cv'), 'Either --fe or --cv must be specified'
 
+avogadro = 6.02214076e23 # Avogadro's number<
 k_B = 1.38064852e-23 # Boltzmann constant in J/K
 T = 310 # Simulation temperature in K (also human body temperature)
 
@@ -33,25 +35,31 @@ if args.fe:
     fe = []
     names = []
     for f in args.fe:
-        data = pd.read_csv(f, sep='\s+', comment='#', header=None)
+        data = pd.read_csv(f, sep='\\s+', comment='#', header=None)
         z.append(data[0].values)
         fe.append(data[1].values)
         names.append(os.path.splitext(os.path.basename(f))[0])
 else:
+    print('Loading collective variable data...')
+    z = []
+    fe = []
+    names = []
+    for f in args.cv:
+        data = pd.read_csv(f, sep='\\s+', comment='#', header=None)
+        z.append(data[4].values)
+        fe.append(data[7].values)
+        names.append(os.path.splitext(os.path.basename(f))[0])
 
-
-
-z = z[0]
+z = np.stack(z, axis=0)
 fe = np.stack(fe, axis=0)
 # Do the preprocessing
-# Step 1: get rid of the edge points by capping the free energy
-fe[:, z < 1.0] = np.max(fe[:, (z > 1.0) * (z < 1.5)])
-fe[:, z > 8.0] = np.max(fe[:, (z > 7.5) * (z < 8.0)])
+# Get rid of the edge points by capping the free energy
+if args.fe:
+    fe[z < 1.0] = np.max(fe[(z > 1.0) * (z < 1.5)])
+    fe[z > 8.0] = np.max(fe[(z > 7.5) * (z < 8.0)])
+    fe = -fe # I think the free energy is negated in the Plumed output...
 
-# Step 2: Shift the free energy such that the maximum is at 0
-fe -= np.max(fe)
-
-# Step 3: Smooth the free energy using a kernel ridge regression
+# Smooth the free energy using a kernel ridge regression
 # I'm not entirely convinced this is necessary, but it's what the authors did
 # So we'll do it too for now...
 # We do this independently for each FE profile
@@ -61,13 +69,16 @@ fe_smooth = np.zeros((fe.shape[0], grid.shape[0]))
 for i in range(fe.shape[0]):
     # Use a kernel ridge regression model
     model = KernelRidge(kernel='rbf', alpha=0.1)
-    model.fit(z[:, None], fe[i, :])
+    model.fit(z[i, :, None], fe[i, :])
     fe_smooth[i, :] = model.predict(grid[:, None])
+    #model = SVR(kernel='rbf', C=1, gamma=0.1)
+    #model.fit(z[i, :, None], fe[i, :])
+    #fe_smooth[i, :] = model.predict(grid[:, None])
 
 # Plot the free energy profiles
 plt.figure()
 for i in range(fe.shape[0]):
-    plt.plot(z, fe[i, :], label='FE ' + names[i])
+    plt.scatter(z[i], fe[i], label='FE ' + names[i])
     plt.plot(grid, fe_smooth[i, :], label='FE ' + names[i] + ' smooth')
 plt.legend()
 plt.xlabel('z')
@@ -76,8 +87,10 @@ plt.savefig('fe_kr.png')
 
 # I'm not entirely sure why the authorse subtract the variance divided by 2k_BT
 # but we'll do it too and see what happens
-fe_smooth = np.mean(fe_smooth, axis=0)# - np.var(fe_smooth, axis=0)/(2.0*k_B*T)
-fe_smooth -= np.max(fe_smooth)
+fe_smooth = np.mean(fe_smooth, axis=0)
+#if args.cv:
+    #fe_smooth = fe_smooth - k_B * T * avogadro * np.var(fe_smooth, axis=0)/(2.0*k_B*T)
+#fe_smooth -= np.max(fe_smooth)
 
 # Fit a neural network to the free energy profile
 # We'll use a 5-fold cross-validation to give us an idea of the error
