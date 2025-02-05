d0f11ccffdb3d76bd550b499855aff75f804b301
Henry Jia <matgms@login01.avon.hpc>
2025-02-05 02:35:22 +0000

    Add new fes

--- common/metadynamics/sum_hills.sh
+++ common/metadynamics/sum_hills.sh
@@ -1,4 +1,4 @@
 # Sum all hills to get a FE profile
 plumed sum_hills --hills HILLS --kt 2.577 --mintozero
 # Same thing but strided so we can check convergence
-plumed sum_hills --hills HILLS --kt 2.577 --mintozero --stride 100
+plumed sum_hills --hills HILLS --kt 2.577 --mintozero --stride 10000
