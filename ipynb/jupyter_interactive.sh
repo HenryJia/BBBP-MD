0043ba132a2b85eb6f776028e32e9c9929a3f920
Hengjian Jia <matgms@login02.sulis.hpc>
2026-02-09 20:59:51 +0000

    Prettify the histograms and add the B3DB overlay on top

--- ipynb/jupyter_interactive.sh
+++ ipynb/jupyter_interactive.sh
@@ -1,3 +1,3 @@
 salloc --account su007-gcs --mem-per-cpu 64000 --time 48:00:00 --partition vhmem -n 64 -N 1
-# ssh -o ExitOnForwardFailure=yes -f -N -R 0.0.0.0:8888:localhost:8888 henry@146.190.238.114 ;
+# ssh -o ExitOnForwardFailure=yes -f -N -R 0.0.0.0:8888:localhost:8888 henry@89.147.108.154 ;
 # python -m pipenv run jupyter notebook --no-browser --ip 0.0.0.0
